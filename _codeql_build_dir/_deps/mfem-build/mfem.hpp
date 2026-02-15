1b3c00120c7bf51f4c89ed87c94a71627b115715
T. C. Raymond <tcraymond@inductivereasoning.com>
2026-02-15 17:25:01 +0000

    Tidying

--- _codeql_build_dir/_deps/mfem-build/mfem.hpp
+++
@@ -1,3 +0,0 @@
-// Auto-generated file.
-#define MFEM_CONFIG_FILE "/home/runner/work/MFEM-ElectroMag/MFEM-ElectroMag/_codeql_build_dir/_deps/mfem-build/config/_config.hpp"
-#include "/home/runner/work/MFEM-ElectroMag/MFEM-ElectroMag/_codeql_build_dir/_deps/mfem-src/mfem.hpp"
